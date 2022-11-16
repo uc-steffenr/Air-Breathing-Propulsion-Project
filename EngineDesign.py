2dbd6fe52b6be3655137b669e3b961d58fbc1389
uc-steffenr <69090586+uc-steffenr@users.noreply.github.com>
2022-11-16 09:01:42 -0500

    added showValues option

--- EngineDesign.py
+++ EngineDesign.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 class EngineDesign:
-    def __init__(self):
+    def __init__(self,**kwargs):
         #########################################
         #            GIVEN VALUES               #
         #########################################
@@ -10,7 +10,7 @@ class EngineDesign:
         self.m = 520 # kg/s
         self.p0 = 1.01 # bar
         self.T0 = 288 # K
-        
+
         self.mc = self.m/(1+self.BPR)
         self.mf = self.m - self.mc
 
@@ -36,6 +36,14 @@ class EngineDesign:
         self.cpa = 1005 # J/(kg-K)
         self.cpg = 1148 # J/(kg-K)
         #########################################
+
+        self.showValues = False
+
+        for key,val in kwargs.items():
+            if key == 'showValues':
+                self.showValues = True
+
+
     
     def compressorSpeedandDimensions(self,Ca,rRatio,Ut):
         #########################################
@@ -50,18 +58,42 @@ class EngineDesign:
         self.To2 = self.T0
         self.po2 = self.p0
 
-
         self.To13 = self.To2*(self.pi_f**((self.gamma_c-1)/(self.eta_inf_f*self.gamma_c)))
         self.po13 = self.pi_f * self.po2
 
-        self.T13 = self.Tt13 - ((self.Ca**2)/(2*self.cpg))
-        self.p13 = self.pt13*(self.T13/self.Tt13)**(self.gamma_c/(self.gamma_c-1))
-        self.rho13 = self.p13/(self.R*self.T13)
+        self.T13 = self.To13 - ((self.Ca**2)/(2*self.cpg))
+        self.p13 = self.po13*(self.T13/self.To13)**(self.gamma_c/(self.gamma_c-1))
+        self.rho13 = (self.p13*1e5)/(self.R*self.T13)
 
-        self.rt = np.sqrt(self.mc/(np.pi(self.rho13*self.Ca*(1-self.rRatio**2))))
+        self.rt = np.sqrt(self.mc/(np.pi*(self.rho13*self.Ca*(1-self.rRatio**2))))
         self.N = self.Ut/(2*np.pi*self.rt)
 
-        self.Vo13 = np.sqrt(self.Ut**2 + self.Ca**2)
+        self.V13t = np.sqrt(self.Ut**2 + self.Ca**2)
+        self.a13 = np.sqrt(self.gamma_c*self.R*self.T13)
+        self.M13t = self.V13t/self.a13
+
+        # now check tip mach number
+        if self.M13t > 1.2:
+            print('###########################')
+            print('TIP MACH NUMBER EXCEEDS 1.2')
+            print('###########################')
+            print('M13t = {0:.2f}'.format(self.M13t))
+
+        if self.showValues:
+            print('###############################')
+            print('COMPRESSOR SPEED AND DIMENSIONS')
+            print('###############################')
+            print('To13 = {0:.2f} K'.format(self.To13))
+            print('po13 = {0:.2f} bar'.format(self.po13))
+            print('T13 = {0:.2f} K'.format(self.T13))
+            print('p13 = {0:.2f} bar'.format(self.p13))
+            print('rho13 = {0:.3f} kg/m^3'.format(self.rho13))
+            print()
+            print('rt = {0:.4f}'.format(self.rt))
+            print('N = {0:.2f} rev/s'.format(self.N))
+            print('V13t = {0:.2f} m/s'.format(self.V13t))
+            print('a13 = {0:.2f} m/s'.format(self.a13))
+            print('M13t = {0:.2f}'.format(self.M13t))
     
     def compressorStageEstimation(self):
         return
