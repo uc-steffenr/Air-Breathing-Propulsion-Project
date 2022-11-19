d647588c8cca53f1fd44bf9d65d8bb5ae545fc52
uc-steffenr <69090586+uc-steffenr@users.noreply.github.com>
2022-11-19 12:07:46 -0500

    finished compressor stage calculations

--- EngineDesign.py
+++ EngineDesign.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sys import exit
 
 class EngineDesign:
     def __init__(self,**kwargs):
@@ -44,10 +45,14 @@ class EngineDesign:
         #########################################
 
         self.showValues = False
+        self.deHallerExit = False
+        # add another variable for constant outer annulus?
 
         for key,val in kwargs.items():
             if key == 'showValues':
-                self.showValues = True
+                self.showValues = val
+            elif key == 'deHallerExit':
+                self.deHallerExit = val
 
 
     
@@ -158,39 +163,18 @@ class EngineDesign:
     def compressorStageDesign(self,numStages):
         self.alpha1_m = []
         self.beta1_m = []
-        # self.Cw1_m = []
-        # self.Vw1_m = []
-        # self.V1_ = []
-        # self.C1 = []
-
         self.alpha2_m = []
         self.beta2_m = []
-        # self.Cw2 = []
-        # self.Vw2 = []
-        # self.V2 = []
-        # self.C2 = []
-
         self.alpha3_m = []
-        self.beta3_m = [] 
-        # self.Cw3 = []
-        # self.Vw3 = []
-        # self.C3 = []
-        # self.V3 = []
 
         self.dToS = (self.To3-self.To13)/numStages
         # need to estimate change in temperature for every stage based on dToS
-        dT_first_last = self.dToS*0.85 # kinda guestimate for first and last delta T
+        dT_first = self.dToS*0.85 # kinda guestimate for first and last delta T
         dT = self.dToS*1.1
 
         print('dToS = {0:.2f} K'.format(self.dToS))
-        print('First and last dToS = {0:.2f} K'.format(dT_first_last))
+        print('First and last dToS = {0:.2f} K'.format(dT_first))
         print('Other stage dToS - {0:.2f} K'.format(dT))
-
-        # work done factors
-        # lam_1 = 0.98
-        # lam_2 = 0.93
-        # lam_3 = 0.88
-        # lam = 0.84
 
         lam = [0.98, 0.93, 0.88, 0.84, 0.83]
 
@@ -200,95 +184,156 @@ class EngineDesign:
 
         for i in range(numStages):
             if i == 0:
-                Cw2 = (self.cpa*dT_first_last)/(lam[0]*self.Um)
+                Cw2 = (self.cpa*dT_first)/(lam[0]*self.Um)
                 a1 = 0
                 b1 = np.arctan(self.Um/self.Ca)
                 b2 = np.arctan((self.Um-Cw2)/self.Ca)
-                a2 = np.arctan(Cw2/np.Ca)
+                a2 = np.arctan(Cw2/self.Ca)
 
                 # check deHaller on rotor
                 deHaller_test = np.cos(b1)/np.cos(b2)
                 if deHaller_test < self.deHaller:
-                    print('STAGE {0} ROTOR FAILS DEHALLER, V1/V2 = {1:.2f}'.format(i,deHaller_test))
-                
-                self.alpha1_m.append(a1)
-                self.alpha2_m.append(a2)
-                self.beta1_m.append(b1)
-                self.beta2_m.append(b2)
+                    print('STAGE {0} ROTOR FAILS DEHALLER AT MEAN, V2/V1 = {1:.2f}'.format(i+1,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
 
-                poRatio = 1 + ((self.eta_inf_c*dT_first_last)/self.To13)**(self.gamma_c/(self.gamma_c-1))
+                poRatio = 1 + ((self.eta_inf_c*dT_first)/self.To13)**(self.gamma_c/(self.gamma_c-1))
                 po_dl = self.po13*poRatio
-                To_dl = self.To13 + dT_first_last
+                To_dl = self.To13 + dT_first
                 Lam_dl = 1 - (Cw2/(2*self.Um))
 
+                self.beta1_m.append(b1)
+                self.beta2_m.append(b2)
+                self.alpha1_m.append(a1)
+                self.alpha2_m.append(a2)
+                
+
             elif i == 1:
-                b1 = np.arctan((self.cpa*dT)/(2*Lam_dl*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
-                b2 = np.arctan(-(self.cpa*dT)/(2*Lam_dl*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
-                a1 = a3_dl = np.arctan((self.Um/self.Ca) - np.tan(b1))
-                self.alpha3_m.append(a3_dl)
+                b1 = np.arctan((self.cpa*dT)/(2*lam[1]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                b2 = np.arctan(-(self.cpa*dT)/(2*lam[1]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                a1 = a3_dl = np.arctan((self.Um/self.Ca) - np.tan(b1))               
                 a2 = np.arctan((self.Um/self.Ca) - np.tan(b2))
-                deHaller_test = np.cos(a2)/np.cos(a3_dl)
+
+                # check deHaller for previous stage
+                deHaller_test = np.cos(self.alpha2_m[i-1])/np.cos(a3_dl)
                 if deHaller_test < self.deHaller:
-                    print('STAGE {0} STATOR FAILS DEHALLER, V3/V3 = {1:.2f}'.format(i-1,deHaller_test))
+                    print('STAGE {0} STATOR FAILS DEHALLER, C3/C2 = {1:.2f}'.format(i,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
+
+                deHaller_test = np.cos(b1)/np.cos(b2)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} ROTOR FAILS DEHALLER, V2/V1 = {1:.2f}'.format(i+1,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
+                
+                Cw1 = self.Ca*np.tan(a1)
+                Cw2 = self.Ca*np.tan(a2)
                 Cwm = (Cw1+Cw2)/2
-                Lam = 1 - Cwm/self.Um
+                Lam_dl = 1 - Cwm/self.Um
                 poRatio = 1 + ((self.eta_inf_c*self.dToS)/(To_dl+self.dToS))
                 po_dl = po_dl*poRatio
                 To_dl = To_dl + self.dToS
+
+                self.beta1_m.append(b1)
+                self.beta2_m.append(b2)
+                self.alpha1_m.append(a1)
+                self.alpha2_m.append(a2)
+                self.alpha3_m.append(a3_dl)
                 
-            elif i == 3:
-                self.dToS = self.dTos - 2
+            elif i == 2:
+                # self.dToS = self.dTos - 2 # lower the delta T_0s a bit so we can get Lambda = 0.5
+                dT = dT - 2
+                Lam_dl = 0.5
+                b1 = np.arctan((self.cpa*dT)/(2*lam[2]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                b2 = np.arctan(-(self.cpa*dT)/(2*lam[2]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                a1 = a3_dl = b2
+                a2 = b1
+
+                deHaller_test = np.cos(self.alpha2_m[i-1])/np.cos(a3_dl)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} STATOR FAILS DEHALLER, C3/C2 = {1:.2f}'.format(i,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
                 
+                deHaller_test = np.cos(b1)/np.cos(b2)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} ROTOR FAILS DEHALLER, V2/V1 = {1:.2f}'.format(i+1,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
+
+                poRatio = 1 + ((self.eta_inf_c*self.dToS)/(To_dl + self.dToS))
+                po_dl = po_dl * poRatio
+                To_dl = To_dl + self.dToS
+
+                self.beta1_m.append(b1)
+                self.beta2_m.append(b2)
+                self.alpha1_m.append(a1)
+                self.alpha2_m.append(a2)
+                self.alpha3_m.append(a3_dl)                
 
             elif i == numStages-1:
-                break
+                poRatio = self.po3/po_dl
+                dT = (poRatio**((self.gamma_c-1)/self.gamma_c) - 1)*(To_dl/self.eta_inf_c)
+
+                b1 = np.arctan((self.cpa*dT)/(2*lam[4]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                b2 = np.arctan(-(self.cpa*dT)/(2*lam[4]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                a1 = a3_dl = b2
+                a2 = b1
+                a3 = 0 # because exit flow is axial
+
+                deHaller_test = np.cos(self.alpha2_m[i-1])/np.cos(a3_dl)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} STATOR FAILS DEHALLER, C3/C2 = {1:.2f}'.format(i,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
+
+                deHaller_test = np.cos(b1)/np.cos(b2)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} ROTOR FAILS DEHALLER, V2/V1 = {1:.2f}'.format(i+1,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
+                
+                # deHaller_test = np.cos(a2)/np.cos(a3)
+                # if deHaller_test < self.deHaller:
+                #     print('STAGE {0} STATOR FAILS DEHALLER, C3/C2 = {1:.2f}'.format(i+1,deHaller_test))
+                #     if self.deHallerExit:
+                #         exit('Design is invalid')    
+
+                self.beta1_m.append(b1)
+                self.beta2_m.append(b2)
+                self.alpha1_m.append(a1)
+                self.alpha2_m.append(a2)
+                self.alpha3_m.append(a3_dl)  
+                self.alpha3_m.append(a3)       
+
             else:
-                break
+                b1 = np.arctan((self.cpa*dT)/(2*lam[3]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                b2 = np.arctan(-(self.cpa*dT)/(2*lam[3]*self.Um*self.Ca) + (self.Um*Lam_dl)/self.Ca)
+                a1 = a3_dl = b2
+                a2 = b1
 
-        # for stages 1 and 2
-        dCw = Cw2 = (self.cpa*dT_first_last)/(lam_1*self.Um)
-        a1 = 0
-        b1 = np.arctan(self.Um/self.Ca)
-        b2 = np.arctan((self.Um-Cw2)/self.Ca)
-        a2 = np.arctan(Cw2/self.Ca)
+                deHaller_test = np.cos(self.alpha2_m[i-1])/np.cos(a3_dl)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} STATOR FAILS DEHALLER, C3/C2 = {1:.2f}'.format(i,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
+                
+                deHaller_test = np.cos(b1)/np.cos(b2)
+                if deHaller_test < self.deHaller:
+                    print('STAGE {0} ROTOR FAILS DEHALLER, V2/V1 = {1:.2f}'.format(i+1,deHaller_test))
+                    if self.deHallerExit:
+                        exit('Design is invalid')
 
-        print('beta1 = {0:.3f}'.format(b1))
-        print('beta2 = {0:.3f}'.format(b2))
-        print('Cw2 = {0:.2f} m/s'.format(Cw2))
+                poRatio = 1 + ((self.eta_inf_c*self.dToS)/(To_dl + self.dToS))
+                po_dl = po_dl * poRatio
+                To_dl = To_dl + self.dToS
 
-        deHaller_test = np.cos(b1)/np.cos(b2)
-        print('V2/V1 = {0:.3f}'.format(deHaller_test))
-
-        if deHaller_test < self.deHaller:
-            print('FIRST STAGE ROTOR FAILS DEHALLER TEST')
-            print('V2/V1 = {0:.3f}'.format(deHaller_test))
-
-        self.alpha1.append(a1)
-        self.beta1.append(b1)
-        self.alpha2.append(a2)
-        self.beta2.append(b2)
-
-        po_ratio = (1 + ((self.eta_inf_c*dT_first_last)/self.To13))**(self.gamma_c/(self.gamma_c-1))
-        po3_1 = self.po13*po_ratio
-        To3_1 = self.To13 + dT_first_last
-
-        Lam = 1 - (Cw2)/(2*self.Um)
-        b1 = np.arctan((self.cpa*dT)/(2*Lam*self.Um*self.Ca) + (self.Um*Lam)/(self.Ca))
-        b2 = np.arctan(-(self.cpa*dT)/(2*Lam*self.Um*self.Ca) + (self.Um*Lam)/(self.Ca))
-        a1 = np.arctan((self.Um/self.Ca) - np.tan(b1))
-        a2 = np.arctan((self.Um/self.Ca) - np.tan(b2))
-
-        Cw1 = self.Ca*np.tan(a1)
-        Cw2 = self.Ca*np.tan(a2)
-
-        deHaller_test = np.cos(a2)/np.cos(a1)
-        print('V2/V1 = {0:.3f}'.format(deHaller_test))
-        if deHaller_test < self.deHaller:
-            print('FIRST STAGE STATOR FAILS DEHALLER TEST')
-            print('V2/V1 = {0:.3f}'.format(deHaller_test))
-
-
-        return
+                self.beta1_m.append(b1)
+                self.beta2_m.append(b2)
+                self.alpha1_m.append(a1)
+                self.alpha2_m.append(a2)
+                self.alpha3_m.append(a3_dl)
     
     def airAngles(self):
         return
